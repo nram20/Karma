21e37af871b37ecde31eda803c2d4c31b9e5e29e
Thomas Wolfe <tewolfe2@gmail.com>
2016-08-29 13:18:50 -0700

    native base added

--- App/Containers/SplashScreen.js
+++ App/Containers/SplashScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { Button } from 'native-base'
 
 // Styles
 import styles from './Styles/PresentationScreenStyle'
@@ -7,6 +8,7 @@ import styles from './Styles/PresentationScreenStyle'
 const SplashScreen = () =>
   <View style={styles.mainContainer}>
     <Text>This is the first screen you see when you're logged in!</Text>
+    <Button onPress={() => { console.log('hi test') }}> Click Me! </Button>
   </View>
 
 export default SplashScreen
