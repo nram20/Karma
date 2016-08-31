911b42edf7c5bd1e7062e5200b5bdc93e8e7a6e1
elliothimmelfarb <elliothimmelfarb@gmail.com>
2016-08-31 11:26:02 -0700

    map

--- App/Reducers/index.js
+++ App/Reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import jobs from './JobReducer'
 import location from './LocationReducer'
+import map from './MapReducer'
 // glue all the reducers together into 1 root reducer
 export default combineReducers({
   jobs,
-  location
+  location,
+  map
 })
 
 // Put reducer keys that you do NOT want stored to persistence here
